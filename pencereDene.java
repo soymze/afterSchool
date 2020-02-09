61e078bb6554d327e14005d727a306f7dba1a3bb
soymze <39768868+soymze@users.noreply.github.com>
2020-02-09 20:56:50 +0300

    Add files via upload

---
+++ pencereDene.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class pencereDene {
+
+	public static void main(String[] args) {
+		
+		karePencere pencere =new karePencere();
+		pencere.setSize(640, 480);
+		pencere.setVisible(true);
+
+	}
+
+}
