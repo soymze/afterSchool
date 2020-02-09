8a8e694a20ac8f0352804aca9ada190d869e6945
soymze <39768868+soymze@users.noreply.github.com>
2020-02-09 21:12:28 +0300

    Add files via upload

---
+++ sayiciIslem.java
@@ -0,0 +1,16 @@
+
+public class sayiciIslem {
+
+	int sayi;
+	boolean bulduMu;
+	
+	public sayiciIslem() {
+		sayi=0;
+		bulduMu= false;
+	}
+	public sayiciIslem(int gelenSayi) {
+		sayi=gelenSayi;
+		bulduMu= false;
+	}
+	
+}
