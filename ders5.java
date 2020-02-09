768dce74f4febdec340a02fba4acf9f3a8805e3d
soymze <39768868+soymze@users.noreply.github.com>
2020-02-09 20:40:48 +0300

    Add files via upload

---
+++ ders5.java
@@ -0,0 +1,11 @@
+
+public class ders5 {
+	public static void main (String args[])
+	{
+		System.out.println("Sonuç=	" +	carpbol(6,5,3));
+		}
+	static int carpbol (int carpan, int carpilan, int bolen)
+	{
+		return (carpan*carpilan)/bolen;
+	}
+}
