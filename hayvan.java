b774aee73e45e2b44775fff4ed24c3dfea239ce7
soymze <39768868+soymze@users.noreply.github.com>
2020-02-09 20:50:06 +0300

    Add files via upload

---
+++ hayvan.java
@@ -0,0 +1,13 @@
+
+public class hayvan {
+	
+	int yas;
+	
+	public void yuru() {
+	System.out.println("Yürüyorum..");	
+	}
+	
+	public void yasSoyle() {
+		System.out.println(yas+ "	yaşındayım..");
+	}
+}
