a391d062df6751c6f345e0f554334d4382f8859e
soymze <39768868+soymze@users.noreply.github.com>
2020-02-09 20:56:22 +0300

    Add files via upload

--- pencere.java
+++ pencere.java
@@ -1,28 +1,34 @@
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 
-public class pencere extends JFrame implements MouseListener {
+public class pencere extends JFrame implements MouseListener,KeyListener {
 
 	private Random rnd;
 	private int x1,y1,x2,y2;
+	private String yazi;
 	public pencere() {
 		super();
 		rnd= new Random();
 		addMouseListener(this);
+		addKeyListener(this);
 		x1=0;
 		x2=0;
 		y1=0;
 		y2=0;
+		yazi="Naber?!";
 	}
 	
 	public void paint(Graphics g) {
 		super.paint(g);
 		
 			g.drawLine(x1, y1,x2,y2);
+			g.drawString(yazi, x1, y1);
 		
 	}
 
@@ -53,6 +59,31 @@ public class pencere extends JFrame implements MouseListener {
 
 	@Override
 	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	    if(e.getKeyCode()==e.VK_BACK_SPACE && yazi.length()>0) {
+				yazi=yazi.substring(0, yazi.length()-1);
+			}
+	    else {
+	    	yazi+=e.getKeyChar();
+	    }
+	    repaint();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
