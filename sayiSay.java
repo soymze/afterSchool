1f13e416d724a7dc718cf6888fcff95d61e1ab2b
soymze <39768868+soymze@users.noreply.github.com>
2020-02-09 20:44:15 +0300

    Add files via upload

---
+++ sayiSay.java
@@ -0,0 +1,23 @@
+
+
+public class sayiSay {
+	
+	public void sayiSay1(int baslangic, int bitis)
+		{
+		int sayi=baslangic;
+		while(sayi<bitis)
+			{
+			System.out.println(sayi);
+			sayi=sayi+1;
+			}
+		}
+	public void sayiSay2(int baslangic, int bitis)
+	{
+	int sayi=baslangic;
+	while(sayi<bitis)
+		{
+		System.out.println(sayi);
+		sayi=sayi+2;
+		}
+	}
+	}
